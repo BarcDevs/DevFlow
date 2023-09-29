987899bd4fc5fe80f9d9e2706df468822301fe06
Bar Cohen <sscohen99@gmail.com>
2023-09-29 14:05:33 +0300

    font setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,18 @@
 import '@styles/theme.css'
 import '@styles/globals.css'
 import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
+import {Inter, Space_Grotesk} from 'next/font/google'
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({
+    subsets: ['latin'],
+    variable: '--font-inter',
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
+const spaceGrotesk = Space_Grotesk({
+    subsets: ['latin'],
+    variable: '--font-inter',
+    weight: ['300', '400', '500', '600', '700']
+})
 
 export const metadata: Metadata = {
     title: 'DevFlow',
@@ -18,7 +27,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
         </html>
     )
 }
