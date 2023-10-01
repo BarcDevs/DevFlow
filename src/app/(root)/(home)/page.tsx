e06f6c4826468890e71845e1a4c0145d82311dd1
Bar Cohen <sscohen99@gmail.com>
2023-10-01 13:49:13 +0300

    moved root page.tsx to home folder

---
+++ src/app/(root)/(home)/page.tsx
@@ -0,0 +1,10 @@
+import {UserButton} from "@clerk/nextjs"
+
+export default function Home() {
+    return (
+        <div>
+            hi
+            <UserButton afterSignOutUrl="/"/>
+        </div>
+    )
+}
