227c5b9f05cf2acc39041becdfcad7a77f49e4e9
Bar Cohen <sscohen99@gmail.com>
2023-10-12 21:02:09 +0300

    added login buttons to sidebar

--- src/components/shared/navbar/LeftSidebar.tsx
+++ src/components/shared/navbar/LeftSidebar.tsx
@@ -1,12 +1,19 @@
+"use client"
+
 import {sidebarLinks} from '@constants'
 import NavItem from '@components/shared/navbar/NavItem'
-import {SignedIn} from '@clerk/nextjs'
+import {SignedIn, SignedOut, useClerk} from '@clerk/nextjs'
+import Link from 'next/link'
+import Image from 'next/image'
+import {Button} from '@components/ui/button'
 
 const LeftSidebar = ({}) => {
+    const {signOut} = useClerk()
+
     return (
         <nav
-            className={'light-border background-light900_dark200 custom-scrollbar sticky left-0 top-0 flex min-h-screen shrink-0 flex-col items-start justify-between gap-4 overflow-y-auto border-r p-6 pb-8 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden'}>
-            <section className={'flex flex-col gap-6'}>
+            className={'light-border background-light900_dark200 custom-scrollbar sticky left-0 top-0 flex h-screen w-fit shrink-0 flex-col items-start justify-between gap-4 overflow-y-auto border-r p-6 pb-8 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]'}>
+            <section className={'flex flex-1 flex-col gap-6'}>
                 {sidebarLinks.map(item => {
                     return (
                         item.label === 'Profile' ?
@@ -17,6 +24,56 @@ const LeftSidebar = ({}) => {
                             )
                     )
                 })}
+            </section>
+
+            <section className={'flex w-full flex-col gap-3'}>
+                <SignedIn>
+                    <Button
+                        className={'text-dark300_light900 flex items-center justify-start gap-4 bg-transparent p-4'}
+                        onClick={() => signOut()}
+                    >
+                        <Image
+                            src={'/assets/icons/logout.svg'}
+                            alt={'logout'}
+                            width={20}
+                            height={20}
+                            className={'invert-colors'}
+                        />
+                        <p className={'base-medium max-lg:hidden'}>
+                            Logout
+                        </p>
+                    </Button>
+                </SignedIn>
+
+                <SignedOut>
+                    <Link href={'/sign-in'}>
+                        <Button
+                            className={'small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'}>
+                            <Image
+                                src={'/assets/icons/account.svg'}
+                                alt={'sign-in'}
+                                width={20}
+                                height={20}
+                                className={'invert-colors lg:hidden'}
+                            />
+                            <span className={'primary-text-gradient max-lg:hidden'}>Log In</span>
+                        </Button>
+                    </Link>
+
+                    <Link href={'/sign-up'}>
+                        <Button
+                            className={'small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'}>
+                            <Image
+                                src={'/assets/icons/sign-up.svg'}
+                                alt={'sign-up'}
+                                width={20}
+                                height={20}
+                                className={'invert-colors lg:hidden'}
+                            />
+                            <span className={'max-lg:hidden'}>Sign Up</span>
+                        </Button>
+                    </Link>
+                </SignedOut>
             </section>
         </nav>
     )
