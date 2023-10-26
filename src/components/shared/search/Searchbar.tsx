ad892e3b56336b08ffc8ad6fdbe8251726a151ef
Bar Cohen <sscohen99@gmail.com>
2023-10-26 21:17:29 +0300

    added properties that needed for local search bar

--- src/components/shared/search/Searchbar.tsx
+++ src/components/shared/search/Searchbar.tsx
@@ -1,18 +1,31 @@
+"use client"
+
 import Image from 'next/image'
 import {Input} from '@components/ui/input'
 
 type SearchbarProps = {
-    type: 'global' | 'local',
-    placeholder: string
+    placeholder: string,
+    imgSrc?: string,
+    iconRight?: boolean,
+    additionalStyle?: string
+} & (GlobalSearchbarProps | LocalSearchbarProps)
+
+type GlobalSearchbarProps = {
+    type: 'global'
 }
-const Searchbar = ({type, placeholder}: SearchbarProps) => {
+
+type LocalSearchbarProps = {
+    type: 'local',
+    route: string,
+}
+const Searchbar = ({type, placeholder, imgSrc, iconRight, additionalStyle}: SearchbarProps) => {
     return (
         <div className={`relative w-full ${type === 'global' && 'max-w-[600px] max-lg:hidden'}`}>
             <div
-                className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4"
+                className={`background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4 ${additionalStyle} ${iconRight && 'flex-row-reverse'}`}
             >
                 <Image
-                    src={'assets/icons/search.svg'}
+                    src={imgSrc || 'assets/icons/search.svg'}
                     alt={'search'}
                     width={24}
                     height={24}
