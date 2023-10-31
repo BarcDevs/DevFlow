6b05f881645a067067091b5c1ce52c646cd61daf
Bar Cohen <sscohen99@gmail.com>
2023-10-31 14:27:57 +0200

    added reusable metric component

---
+++ src/components/shared/Metric.tsx
@@ -0,0 +1,41 @@
+import Icon from '@components/shared/Icon'
+import Image from 'next/image'
+
+type MetricProps = {
+    image: string
+    value: string | number
+    title: string
+    href?: string
+    isAuthor?: boolean
+    textStyles: string
+}
+
+const Metric = ({image, title, value, isAuthor, textStyles}: MetricProps) => {
+    return (
+        <div className={'flex-center flex-row flex-wrap gap-1'}>
+            {isAuthor ?
+                <Image
+                    src={image}
+                    alt={'avatar'}
+                    width={20}
+                    height={20}
+                    className={'rounded-full'}
+                /> :
+                <Icon
+                    name={image}
+                    size={16}
+                />}
+
+            <p className={`${textStyles} flex items-center gap-1`}>
+                {value}
+
+                <span className={`small-regular line-clamp-1 ${isAuthor ? 'max-sm:hidden' : ''}`}
+                >
+                    {title}
+                </span>
+            </p>
+        </div>
+    )
+}
+
+export default Metric
