6a21cfe3c1af2784cda9268a77d291b147dfed86
Bar Cohen <sscohen99@gmail.com>
2023-10-31 18:32:15 +0200

    question form: added form schema with zod

---
+++ src/lib/validations.ts
@@ -0,0 +1,9 @@
+import * as z from "zod"
+
+export const QuestionsSchema = z.object({
+    title: z.string().min(5).max(130),
+    body: z.string().min(20),
+    tags: z.array(
+        z.string().min(1).max(15)
+    ).min(1).max(3)
+})
