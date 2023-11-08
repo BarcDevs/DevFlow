f2bc788620d9ecc4d4913f12f240c46ccf9c8de0
Bar Cohen <sscohen99@gmail.com>
2023-11-08 15:26:57 +0200

    question form: route page modified according to form component's needs

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,6 +1,21 @@
 import QuestionForm from '@components/shared/forms/QuestionForm'
+import {redirect} from 'next/navigation'
+import {getUserByClerkId} from '@lib/actions/user.action'
+import {UserDocument} from '@lib/db/user.model'
 
-const Page = ({}) => {
+const Page = async ({}) => {
+    // const {userId} = auth()
+    const userId = '12345'
+    if (!userId) redirect('/sign-in')
+
+    let mongoUser: UserDocument | null = null
+    try {
+        mongoUser = await getUserByClerkId({clerkID: userId})
+    } catch (e) {
+        console.error(e)
+    }
+
+
     return (
         <>
             <h1 className={'h1-bold text-dark100_light900'}>
@@ -8,7 +23,7 @@ const Page = ({}) => {
             </h1>
 
             <section className={'mt-9'}>
-                <QuestionForm/>
+                <QuestionForm type={'create'} userId={JSON.stringify(mongoUser?._id)}/>
             </section>
         </>
     )
